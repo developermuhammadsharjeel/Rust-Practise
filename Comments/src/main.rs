d8d202e9c498f9b2611be0b50cc32546f50acadf
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-08-30 10:04:00 +0500

    Commets in Rust

--- Comments/src/main.rs
+++ Comments/src/main.rs
@@ -1,3 +1,15 @@
+// we basiclly initialize comments in tust using // 
+//these are sinle line comments
+
+/*
+These are multiline comments
+*/
+
+// we can press ctrl + / after selecting lines of code then that lines will be commented
+
+/*
 fn main() {
     println!("Hello, world!");
-}
+
+    }
+    */
