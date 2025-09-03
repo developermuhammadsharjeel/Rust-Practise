41cd62246ef493f93d93c6bf467387f03068b677
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-09-03 21:43:07 +0500

    Operators

--- operators/src/main.rs
+++ operators/src/main.rs
@@ -1,3 +1,29 @@
+
+// ................. Operators in Rust
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+
+
+
 fn main() {
-    println!("Hello, world!");
-}
+  let mut x = 10;
+  println!("Start: {}", x);
+
+  x += 5;
+  println!("After += 5: {}", x);
+
+  x -= 2;
+  println!("After -= 2: {}", x);
+
+  x *= 2;
+  println!("After *= 2: {}", x);
+
+  x /= 3;
+  println!("After /= 3: {}", x);
+
+  x %= 4;
+  println!("After %= 4: {}", x); }
+
+  //..................................................
