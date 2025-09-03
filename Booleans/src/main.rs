6c25429e96e29f132cf71a70d352e47c231e105e
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-09-03 21:57:28 +0500

    Booleans

---
+++ Booleans/src/main.rs
@@ -0,0 +1,13 @@
+// Rust Booleans
+// Booleans represent values that are either true or false.
+
+
+
+//example
+fn main() {
+    let is_programming_fun: bool = true;
+let is_fish_tasty: bool = false;
+
+println!("Is Programming Fun? {}", is_programming_fun);
+println!("Is Fish Tasty? {}", is_fish_tasty);
+}
