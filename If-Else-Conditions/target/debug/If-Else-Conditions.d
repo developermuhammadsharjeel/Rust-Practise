4dc9d58e748034cdba9ea8d3fe84f95a41ad67bb
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-09-09 10:21:37 +0500

    IF / ELSE Conditions in Rust

---
+++ If-Else-Conditions/target/debug/If-Else-Conditions.d
@@ -0,0 +1,1 @@
+E:\Rust\Rust-Practise\If-Else-Conditions\target\debug\If-Else-Conditions.exe: E:\Rust\Rust-Practise\If-Else-Conditions\src\main.rs
---
+++ If-Else-Conditions/target/debug/deps/If_Else_Conditions.d
@@ -0,0 +1,5 @@
+E:\Rust\Rust-Practise\If-Else-Conditions\target\debug\deps\If_Else_Conditions.d: src\main.rs
+
+E:\Rust\Rust-Practise\If-Else-Conditions\target\debug\deps\If_Else_Conditions.exe: src\main.rs
+
+src\main.rs:
