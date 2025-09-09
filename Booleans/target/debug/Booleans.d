694026ca25135ae5f6aede45998038f0f5071c17
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-09-09 10:22:24 +0500

    Updates

---
+++ Booleans/target/debug/Booleans.d
@@ -0,0 +1,1 @@
+E:\Rust\Rust-Practise\Booleans\target\debug\Booleans.exe: E:\Rust\Rust-Practise\Booleans\src\main.rs
---
+++ Booleans/target/debug/deps/Booleans.d
@@ -0,0 +1,5 @@
+E:\Rust\Rust-Practise\Booleans\target\debug\deps\Booleans.d: src\main.rs
+
+E:\Rust\Rust-Practise\Booleans\target\debug\deps\Booleans.exe: src\main.rs
+
+src\main.rs:
